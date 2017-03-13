01d2d71d30b8a3a70446f4941d2a8eb9f939353e
ghi <gustavoghioldi@gmail.com>
2017-03-13 17:36:26 -0300

    me corto el mambo damian... despues sigo

---
+++ src/app/pages/suport/config/suportConfigManagersCtrl.js
@@ -0,0 +1,17 @@
+/**
+ * @author a.demeshko
+ * created on 21.01.2016
+ */
+(function() {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.suport')
+        .controller('SuportConfigManagersCtrl', SuportConfigManagersCtrl);
+
+    /** @ngInject */
+    function SuportConfigManagersCtrl($scope) {
+        console.log('SuportConfigManagersCtrl...');
+
+    }
+
+})();
---
+++ src/app/pages/suport/config/suportConfigTypesCtrl.js
@@ -0,0 +1,17 @@
+/**
+ * @author a.demeshko
+ * created on 21.01.2016
+ */
+(function() {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.suport')
+        .controller('SuportConfigTypesCtrl', SuportConfigTypesCtrl);
+
+    /** @ngInject */
+    function SuportConfigTypesCtrl($scope) {
+        console.log('SuportConfigTypesCtrl...');
+
+    }
+
+})();
