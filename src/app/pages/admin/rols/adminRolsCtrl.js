f2b2c70f9a950ea9ded05baf7a849905846694e8
ghi <gustavoghioldi@gmail.com>
2017-03-18 14:48:16 -0300

    view de los roles

--- src/app/pages/admin/rols/adminRolsCtrl.js
+++ src/app/pages/admin/rols/adminRolsCtrl.js
@@ -25,15 +25,16 @@
             });
         }
 
-        $scope.view = function(key) {
+        $scope.view = function(item) {
 
             $uibModal.open({
                 animation: true,
                 templateUrl: 'app/pages/admin/widgets/rols.view.modal.html',
                 size: 'lg',
+                controller: function($scope) {
+                    $scope.rol = item;
+                }
             });
-
-
         }
 
         $scope.delete = function(key) {
