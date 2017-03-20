6af569dd18c3b3ada37ebb7cf56c02b16d4f0c91
gustavoghioldi <gustavoghioldi@gmail.com>
2017-03-20 15:28:29 -0300

    andan suport / status y tipos}

--- src/app/pages/suport/config/suportConfigStatusCtrl.js
+++ src/app/pages/suport/config/suportConfigStatusCtrl.js
@@ -12,6 +12,25 @@
     function SuportConfigStatusCtrl($scope) {
         console.log('SuportConfigStatusCtrl...');
 
+        	  suportRef.child('/status').on('value', function	(ss){
+        	$scope.status = ss.val();
+        	console.log($scope.status); 
+        });
+
+         $scope.save = function(){
+        	console.log("Guardo el tipo");
+        	suportRef.child("/status").push({
+                        name: $scope.name
+                    });
+        }
+
+        $scope.delete = function (key){
+        	console.log(key);
+        	if(confirm("esta Seguri que desea borrar este status de tikets?")){
+        	console.log(suportRef.child("status/"+key).remove());
+        }
+        }
+
     }
 
 })();
