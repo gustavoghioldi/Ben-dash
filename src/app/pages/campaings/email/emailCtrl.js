4734d4ba157c5ac2546d9ad128b8d989daa88f59
Gustavo Ghioldi <gustavoghioldi@gmail.com>
2017-04-18 11:43:49 -0300

    me gusta este editos

---
+++ src/app/pages/campaings/email/emailCtrl.js
@@ -0,0 +1,39 @@
+
+
+/**
+ * @author a.demeshko
+ * created on 18.01.2016
+ */
+(function () {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.campaings.email')
+        .controller('EmailCtrl', EmailCtrl);
+
+    /** @ngInject */
+    function EmailCtrl($scope, $state, $uibModal) {
+        $scope.tinymceModel = 'Initial content';
+
+        $scope.getContent = function () {
+            console.log('Editor content:', $scope.tinymceModel);
+        };
+
+        $scope.setContent = function () {
+            $scope.tinymceModel = 'Time: ' + (new Date());
+        };
+
+        $scope.tinymceOptions = {
+            plugins: ["advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
+                "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+                "table contextmenu directionality emoticons template textcolor paste fullpage textcolor colorpicker textpattern"],
+            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code',
+            templates: [
+                { title: 'Some title 1', description: 'Some desc 1', content: '<h1>sdasdasd</h1>' },
+                { title: 'Some title 2', description: 'Some desc 2', url: 'development.html' }
+            ]
+        };
+
+    }
+
+
+})();
