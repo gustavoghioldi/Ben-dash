2dff17c757ef2d7a27f895d84f17b0fce3b9e35f
Gustavo Ghioldi <gustavoghioldi@gmail.com>
2017-04-20 10:20:46 -0300

    algunos manotazos de ahogado

--- src/app/pages/transactions/init/transactionsSalesCtrl.js
+++ src/app/pages/transactions/init/transactionsSalesCtrl.js
@@ -5,11 +5,27 @@
         .controller('TransactionsSalesCtrl', TransactionsSalesCtrl);
 
     /** @ngInject */
-    function TransactionsSalesCtrl($scope, $state, $stateParams) {
+    function TransactionsSalesCtrl($scope, $state, $stateParams,  $uibModal) {
         console.log('TransactionsSalesCtrl....');
         transactionsRef.on('value', function(ss) {
             $scope.transactions = ss.val();
             console.log($scope.transactions);
         });
+
+        $scope.getMemeber = function(memberID){
+            membersRef.child("-KiA606z5X3VbTcqZFyE").on('value', function(ss){
+                var templateModal = '<p> email: '+ss.val().email+'</p><br />';
+                templateModal += '<p> Nombre: '+ss.val().firstname+'</p><br />';
+                templateModal += '<p> Apellido: '+ss.val().lastname+'</p><br />';
+                templateModal += '<p> DNI: '+ss.val().dni+'</p><br />';
+                templateModal += '<p> Provincia: '+ss.val().state+'</p><br />';
+                templateModal += '<p> Ciudad: '+ss.val().city+'</p><br />';
+                $uibModal.open({
+                    animation: true,
+                    template: templateModal,
+                    size: 'md'
+                });
+            });
+        }
     }
 })();
